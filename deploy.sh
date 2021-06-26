b9fc4ec2689e919d28e583670033a81f2a165e7f
sullo <sullo@cirt.net>
2021-06-26 09:26:15 -0400

    Site Updates

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-jekyll build
+bundle exec jekyll build
 cp -R _site/* ../sullo.github.io/
-#cp -R _posts/ ../sullo.github.io/
-#cp -R _layouts/ ../sullo.github.io/
-#cp -R includes/ ../sullo.github.io/
-#cp *html ../sullo.github.io/
 cd ../sullo.github.io/ && git add -A && git commit -m "Site Updates" && git push
 
